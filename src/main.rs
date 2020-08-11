5087f43621504d7e122059f7c20744c42c77f4e3
Rafael Trevisan <rafa.trevi@hotmail.com>
2020-08-11 11:28:33 -0300

    Added Dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rumqttc::{EventLoop, MqttOptions, QoS, Request, Subscribe};
+use rumqttc::{EventLoop, MqttOptions, QoS, Request, Subscribe,Incoming};
 use std::time::Instant;
 use std::env;
 
@@ -7,14 +7,17 @@ async fn main() {
     let args :Vec<String> = env::args().collect();
     
     let expected = &args[1];
+    let adress = &args[2];
+
     let max = expected.parse::<u16>().unwrap() + 3;  
 
-    let mut mqttoptions = MqttOptions::new("Reciever", "localhost", 1883);
+    let mut mqttoptions = MqttOptions::new("Reciever", adress, 1883);
         mqttoptions.set_keep_alive(10000);
+        mqttoptions.set_max_packet_size(2);
     let mut eventloop = EventLoop::new(mqttoptions, 10).await;
     let requests_tx = eventloop.handle();
 
-    let subscription = Subscribe::new("hello", QoS::AtMostOnce);
+    let subscription = Subscribe::new("hello", QoS::AtLeastOnce);
     let _ = requests_tx.send(Request::Subscribe(subscription)).await;
     let mut counter : u16 = 0;
 
@@ -33,11 +36,26 @@ async fn main() {
         let now = Instant::now();    
         loop {
             let incoming = eventloop.poll().await;
-            println!("Incoming = {:?}", incoming);
-            counter += 1;
-            if counter >= max {break}
+            //println!("{}", counter);
+
+            if counter >= max {
+                if let Ok((Some(m),_)) = incoming {
+                let res = match m {
+                Incoming::Publish(p) => Some(p.payload),
+                _ => None,
+                };
+                  
+                if let Some(txt) = res {
+                let s = txt.iter().map(|e| *e as char).collect::<String>();
+                println!("{}",s);
+                 }
+             }
+                
+                break}
+                counter += 1;
         }
         println!("{}",now.elapsed().as_millis());
+
         }
      }    
-}
+    }
